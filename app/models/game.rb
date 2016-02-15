affee0fe74fc7bfbfd117d79f47e002cdd21e1f0
Lennart Giesen <lennartgiesen@Lennarts-MacBook-Pro-2.local>
2016-02-15 10:16:01 +0100

    Scaffold.css angepasst, Post.css eingefügt, custom.css angepasst, Platzhalter eingefügt, seed.rb angepasst.

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,15 +11,21 @@ class Game < ActiveRecord::Base
   has_many :game_platforms
   has_many :platforms, through: :game_platforms
 
-  # Auskommentiert, weil es unter Windows noch Probleme gibt.
+  # Auskommentiert, weil unter Windows Probleme beim hochladen von Bildern besteht.
   #validates_attachment :image, :presence => true,
   #                     :content_type => { :content_type => /\Aimage\/.*\Z/ }
 
+  # Erst einmal wird das Bild nicht mehr validiert.
   do_not_validate_attachment_file_type :image
 
-
+  validates :link, format: %r|\Ahttp(s?)://www.amazon.de/|
+  validates :genres, :platforms, :title, :developer, presence: true
 
   def self.search(search)
     where("title LIKE ? OR description LIKE ? OR developer LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+  end
+
+  def self.stars()
+    0..4
   end
 end
