8949eaa830d6a81e9155d0a1d0914d8aba499617
Nicolas Burchert <nicolas.burchert@gmail.com>
2016-02-18 14:12:36 +0100

    Gem Datepicker wurde hinzugefügt. ReleaseDate wurde hinzugefügt.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216111319) do
+ActiveRecord::Schema.define(version: 20160218125233) do
 
   create_table "game_genres", force: :cascade do |t|
     t.integer  "game_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160216111319) do
     t.string   "link"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.date     "releaseDate"
   end
 
   create_table "genres", force: :cascade do |t|
