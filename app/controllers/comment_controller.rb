688a5ce381886ae79ef5487bea8d2e443c531667
Lennart Giesen <lennart.giesen@outlook.com>
2016-02-23 12:56:09 +0100

    Kleinere Anpassungen

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,19 +1,25 @@
 class CommentController < ApplicationController
   before_action :authenticate_user!, only: [:destroy, :new]
+
+  #GET /comment/new
   def _new
     @comment = Comment.new
   end
 
+  # PUT /comment/notify
+  # PUT /comment/notify.json
   def notify
-    @comment = Comment.find(params[:id])
-    @comment.notify = true
-     respond_to do |format|
-       if @comment.save
-         format.html { redirect_to game_path(:id => @comment.game_id), notice: 'Kommentar wurde gemeldet'}
-       else
-         format.html { render :new }
-       end
-     end
+    if params[:id]
+      @comment = Comment.find(params[:id])
+      @comment.notify = true
+    end
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to game_path(:id => @comment.game_id), notice: 'Kommentar wurde gemeldet' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   # POST /comment
@@ -31,6 +37,8 @@ class CommentController < ApplicationController
   end
 
 
+  # DELETE /comment/destroy/1
+  # DELETE /comment/destroy/1.json
   def destroy
     if params[:id]
       @comment = Comment.find(params[:id])
